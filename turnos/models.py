2595f0fc76ceec52875fcb836301d59a274a3333
joaquín sauan <sauanjoaquin15@gmail.com>
2026-04-16 02:09:44 -0300

    corrercion

--- turnos/models.py
+++ turnos/models.py
@@ -22,9 +22,10 @@ class meta:
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField()
-
+    apellido = models.CharField(max_length=50)
     def __str__(self):
         return self.nombre
+    
 
 
 class Servicio(models.Model):
