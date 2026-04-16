31cfa2eb1e4d76b5dbe41a0939601e653545017e
joaquín sauan <sauanjoaquin15@gmail.com>
2026-04-16 02:11:38 -0300

    correcion

--- turnos/models.py
+++ turnos/models.py
@@ -22,7 +22,7 @@ class meta:
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField()
-    apellido = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50, default="Sin apellido")
     def __str__(self):
         return self.nombre
     
